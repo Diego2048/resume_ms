ff29e5c9713fddd26f52f9606a386b3722d0d946
wurui <inosin7000@gmail.com>
2013-07-10 13:45:40 +0800

    使用内置helper获取表名

--- admin/controllers/accounts.rb
+++ admin/controllers/accounts.rb
@@ -1,13 +1,13 @@
 Newresume::Admin.controllers :accounts do
 
   get :index do
-    @title = t('models.account.name')
+    @title = mt(:account)
     @accounts = Account.all
     render 'accounts/index'
   end
 
   get :new do
-    @title = pat(:new_title, model: t('models.account.name'))
+    @title = pat(:new_title, model: mt(:admin))
     @account = Account.new
     render 'accounts/new'
   end
@@ -15,76 +15,76 @@ Newresume::Admin.controllers :accounts do
   post :create do
     @account = Account.new(account_params)
     if @account.save
-      @title = pat(:create_title, model: "#{t('models.account.name')} #{@account.id}")
-      flash[:success] = pat(:create_success, model: t('models.account.name'))
+      @title = pat(:create_title, model: "#{mt(:account)} #{@account.id}")
+      flash[:success] = pat(:create_success, model: mt(:account))
       params[:save_and_continue] ? redirect(url(:accounts, :index)) : redirect(url(:accounts, :edit, id: @account.id))
     else
-      @title = pat(:create_title, model: t('models.account.name'))
-      flash.now[:error] = pat(:create_error, model: t('models.account.name'))
+      @title = pat(:create_title, model: mt(:account))
+      flash.now[:error] = pat(:create_error, model: mt(:account))
       render 'accounts/new'
     end
   end
 
   get :edit, :with => :id do
-    @title = pat(:edit_title, model: "#{t('models.account.name')} #{params[:id]}")
+    @title = pat(:edit_title, model: "#{mt(:account)} #{params[:id]}")
     @account = Account.find(params[:id])
     if @account
       render 'accounts/edit'
     else
-      flash[:warning] = pat(:create_error, model: t('models.account.name'), id: "#{params[:id]}")
+      flash[:warning] = pat(:create_error, model: mt(:account), id: "#{params[:id]}")
       halt 404
     end
   end
 
   put :update, :with => :id do
-    @title = pat(:update_title, model: "#{t('models.account.name')} #{params[:id]}")
+    @title = pat(:update_title, model: "#{mt(:account)} #{params[:id]}")
     @account = Account.find(params[:id])
     if @account
       if @account.update_attributes(account_params)
-        flash[:success] = pat(:update_success, model: t('model.account.name'), id:  "#{params[:id]}")
+        flash[:success] = pat(:update_success, model: mt(:account), id:  "#{params[:id]}")
         params[:save_and_continue] ?
           redirect(url(:accounts, :index)) :
           redirect(url(:accounts, :edit, id: @account.id))
       else
-        flash.now[:error] = pat(:update_error, model: t('models.account.name'))
+        flash.now[:error] = pat(:update_error, model: mt(:account))
         render 'accounts/edit'
       end
     else
-      flash[:warning] = pat(:update_warning, model: t('models.account.name'), id: "#{params[:id]}")
+      flash[:warning] = pat(:update_warning, model: mt(:account), id: "#{params[:id]}")
       halt 404
     end
   end
 
   delete :destroy, :with => :id do
-    @title = t('models.account.name')
+    @title = mt(:account)
     account = Account.find(params[:id])
     if account
       if account != current_account && account.destroy
-        flash[:success] = pat(:delete_success, model: t('models.account.name'), id: "#{params[:id]}")
+        flash[:success] = pat(:delete_success, model: mt(:account), id: "#{params[:id]}")
       else
-        flash[:error] = pat(:delete_error, model: t('models.account.name'))
+        flash[:error] = pat(:delete_error, model: mt(:account))
       end
       redirect url(:accounts, :index)
     else
-      flash[:warning] = pat(:delete_warning, model: t('models.account.name'), id: "#{params[:id]}")
+      flash[:warning] = pat(:delete_warning, model: mt(:account), id: "#{params[:id]}")
       halt 404
     end
   end
 
   delete :destroy_many do
-    @title = t('models.account.name')
+    @title = mt(:account)
     unless params[:account_ids]
-      flash[:error] = pat(:destroy_many_error, model: t('models.account.name'))
+      flash[:error] = pat(:destroy_many_error, model: mt(:account))
       redirect(url(:accounts, :index))
     end
     ids = params[:account_ids].split(',').map(&:strip).map(&:to_i)
     accounts = Account.find(ids)
     
     if accounts.include? current_account
-      flash[:error] = pat(:delete_error, model: t('models.account.name'))
+      flash[:error] = pat(:delete_error, model: mt(:account))
     elsif Account.destroy accounts
     
-      flash[:success] = pat(:destroy_many_success, model: t('models.account.name'), :ids => "#{ids.to_sentence}")
+      flash[:success] = pat(:destroy_many_success, model: mt(:account), :ids => "#{ids.to_sentence}")
     end
     redirect url(:accounts, :index)
   end
--- admin/controllers/users.rb
+++ admin/controllers/users.rb
@@ -1,13 +1,13 @@
 Newresume::Admin.controllers :users do
 
   get :index do
-    @title = t('models.user.name')
+    @title = mt(:user)
     @users = User.all
     render 'users/index'
   end
 
   get :new do
-    @title = pat(:new_title, :model => t('models.user.name'))
+    @title = pat(:new_title, :model => mt(:user))
     @user = User.new
     render 'users/new'
   end
@@ -15,66 +15,66 @@ Newresume::Admin.controllers :users do
   post :create do
     @user = User.new(user_params)
     if @user.save
-      @title = pat(:create_title, :model => "#{t('models.user.name')} #{@user.id}")
-      flash[:success] = pat(:create_success, :model => t('models.user.name'))
+      @title = pat(:create_title, :model => "#{mt(:user)} #{@user.id}")
+      flash[:success] = pat(:create_success, :model => mt(:user))
       params[:save_and_continue] ? redirect(url(:users, :index)) : redirect(url(:users, :edit, :id => @user.id))
     else
-      @title = pat(:create_title, :model => t('models.user.name'))
-      flash.now[:error] = pat(:create_error, :model => t('models.user.name'))
+      @title = pat(:create_title, :model => mt(:user))
+      flash.now[:error] = pat(:create_error, :model => mt(:user))
       render 'users/new'
     end
   end
 
   get :edit, :with => :id do
-    @title = pat(:edit_title, :model => "#{t('models.user.name')} #{params[:id]}")
+    @title = pat(:edit_title, :model => "#{mt(:user)} #{params[:id]}")
     @user = User.find(params[:id])
     if @user
       render 'users/edit'
     else
-      flash[:warning] = pat(:create_error, :model => t('models.user.name'), :id => "#{params[:id]}")
+      flash[:warning] = pat(:create_error, :model => mt(:user), :id => "#{params[:id]}")
       halt 404
     end
   end
 
   put :update, :with => :id do
-    @title = pat(:update_title, :model => "#{t('models.user.name')} #{params[:id]}")
+    @title = pat(:update_title, :model => "#{mt(:user)} #{params[:id]}")
     @user = User.find(params[:id])
     if @user
       if @user.update_attributes(user_params)
-        flash[:success] = pat(:update_success, :model => t('models.user.name'), :id =>  "#{params[:id]}")
+        flash[:success] = pat(:update_success, :model => mt(:user), :id =>  "#{params[:id]}")
         params[:save_and_continue] ?
           redirect(url(:users, :index)) :
           redirect(url(:users, :edit, :id => @user.id))
       else
-        flash.now[:error] = pat(:update_error, :model => t('models.user.name'))
+        flash.now[:error] = pat(:update_error, :model => mt(:user))
         render 'users/edit'
       end
     else
-      flash[:warning] = pat(:update_warning, :model => t('models.user.name'), :id => "#{params[:id]}")
+      flash[:warning] = pat(:update_warning, :model => mt(:user), :id => "#{params[:id]}")
       halt 404
     end
   end
 
   delete :destroy, :with => :id do
-    @title = t('models.user.name')
+    @title = mt(:user)
     user = User.find(params[:id])
     if user
       if user.destroy
-        flash[:success] = pat(:delete_success, :model => t('models.user.name'), :id => "#{params[:id]}")
+        flash[:success] = pat(:delete_success, :model => mt(:user), :id => "#{params[:id]}")
       else
-        flash[:error] = pat(:delete_error, :model => t('models.user.name'))
+        flash[:error] = pat(:delete_error, :model => mt(:user))
       end
       redirect url(:users, :index)
     else
-      flash[:warning] = pat(:delete_warning, :model => t('models.user.name'), :id => "#{params[:id]}")
+      flash[:warning] = pat(:delete_warning, :model => mt(:user), :id => "#{params[:id]}")
       halt 404
     end
   end
 
   delete :destroy_many do
-    @title = t('models.user.name')
+    @title = mt(:user)
     unless params[:user_ids]
-      flash[:error] = pat(:destroy_many_error, :model => t('models.user.name'))
+      flash[:error] = pat(:destroy_many_error, :model => mt(:user))
       redirect(url(:users, :index))
     end
     ids = params[:user_ids].split(',').map(&:strip).map(&:to_i)
@@ -82,7 +82,7 @@ Newresume::Admin.controllers :users do
     
     if User.destroy users
     
-      flash[:success] = pat(:destroy_many_success, :model => t('models.user.name'), :ids => "#{ids.to_sentence}")
+      flash[:success] = pat(:destroy_many_success, :model => mt(:user), :ids => "#{ids.to_sentence}")
     end
     redirect url(:users, :index)
   end
